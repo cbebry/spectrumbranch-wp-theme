571b6914ee10046b856ab3900fa0811460b7a51b
Rachel Hirshan <acergriseum919@gmail.com>
2023-12-31 19:54:33 -0500

    Add all bootscore theme files

---
+++ bootscore-main/custom/homepage-custom.php
@@ -0,0 +1,42 @@
+<!-- Custom Homepage Layout -->
+<section id="home-banner">
+	<h1 class="h1">All about two crazy cat brothers: Stitch and Coda</h1>
+    <div class="btn-section">
+    	<a href="https://stitchandcoda.com/category/recipes/" class="btn btn-outline-light">View All Recipes</a>
+    </div>
+</section>
+
+<section id="home-steps" class="content-section">
+	<div class="container">
+    	<h2 class="h2 text-center">The Helping Hands Kitchen 3-Step Process</h2>
+		<div class="row g-md-4 g-3">
+        	<div class="col-md-6 order-first">
+				<img src="http://stitchandcoda.com/wp-content/uploads/2023/10/meatballs-plated-pasta-bread-1024x576.jpg" alt="" class="home-step-img" />         
+         	</div>
+         	<div class="col-md-6 order-first">
+         		<h3 class="h3">
+                	<span class="step-title">Step 1</span>
+                    <span class="step-description">We Explore Loosely Prescribed Recipes</span>
+                </h3>
+         	</div>
+         	<div class="col-md-6  order-md-2">
+				<img src="http://stitchandcoda.com/wp-content/uploads/2023/10/meatballs-plated-pasta-bread-1024x576.jpg" alt="" class="home-step-img" />
+         	</div>      
+        	<div class="col-md-6 order-md-1">
+         		<h3 class="h3">
+                	<span class="step-title">Step 2</span>
+                    <span class="step-description">We Cook The Recipes Ourselves</span>
+                </h3>
+         	</div>
+            <div class="col-md-6 order-last">
+				<img src="http://stitchandcoda.com/wp-content/uploads/2023/10/meatballs-plated-pasta-bread-1024x576.jpg" alt="" class="home-step-img" />         
+         	</div>
+         	<div class="col-md-6 order-last">
+         		<h3 class="h3">
+                	<span class="step-title">Step 3</span> 
+                	<span class="step-description">We Provide Feedback Based on the Results</span>
+                </h3>
+         	</div>
+        </div>
+    </div>
+</section>
